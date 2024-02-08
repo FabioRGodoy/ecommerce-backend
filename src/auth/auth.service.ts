ae369c5bb6f044d2e88ec4ab71de859ffea705a8
Fabio <fabiogodoy955@icloud.com>
2024-02-08 17:54:09 -0300

    feat(auth): implement login generate jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
+import { User } from '@prisma/client';
+import { UserPayload } from './models/UserPayload';
+import { JwtService } from '@nestjs/jwt';
+import { UserToken } from './models/UserToken';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jtwService: JwtService,
+  ) {}
+
+  login(user: User): UserToken {
+    const payload: UserPayload = {
+      sub: user.id,
+      email: user.email,
+      name: user.name,
+    };
+
+    const jwtToken = this.jtwService.sign(payload);
+
+    return {
+      access_token: jwtToken,
+    };
+  }
+
   async validateUser(email: string, password: string) {
     const user = await this.userService.findByEmail(email);
 
