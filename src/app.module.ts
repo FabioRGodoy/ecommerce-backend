6052c5b608d522223f872cb8828180289d931515
Fabio <fabiogodoy955@icloud.com>
2024-02-08 18:21:51 -0300

    feat(auth): implement jwt validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,19 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
 import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
 @Module({
   imports: [PrismaModule, UserModule, AuthModule],
   controllers: [AppController, UserController],
-  providers: [AppService, UserService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
