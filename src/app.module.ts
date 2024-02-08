cc6e53ca363faf1cd4fc5d7384e1e03dff754d91
Fabio <fabiogodoy955@icloud.com>
2024-02-08 16:34:43 -0300

    feat(auth): create module, controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, UserModule],
+  imports: [PrismaModule, UserModule, AuthModule],
   controllers: [AppController, UserController],
   providers: [AppService, UserService],
 })
