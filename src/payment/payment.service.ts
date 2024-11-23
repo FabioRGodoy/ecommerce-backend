13a5d47e1246033449cc59331ce5b2e09727fc43
Fabio <fabiogodoy955@icloud.com>
2024-11-23 14:40:40 -0300

    fix(Paymets): implement try catch and errors

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -1,37 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import axios from 'axios';
+import { CreateCustomerDto } from './dto/create-customer.dto';
+import { CreateChargeDto } from './dto/create-charger.dto';
 
 @Injectable()
 export class PaymentService {
   private readonly asaasApiUrl = process.env.ASSAS_API_BASE_ADDRESS;
   private readonly asaasApiKey = process.env.ASAAS_API_KEY;
 
-  async createCustomer(customerData: any) {
-    const response = await axios.post(
-      `${this.asaasApiUrl}/customers`,
-      customerData,
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          access_token: this.asaasApiKey,
+  async createCustomer(customerData: CreateCustomerDto) {
+    try {
+      const response = await axios.post(
+        `${this.asaasApiUrl}/customers`,
+        customerData,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            access_token: this.asaasApiKey,
+          },
         },
-      },
-    );
-    return response.data;
+      );
+
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(
+        'Erro ao criar o cliente',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  async createCharge(chargeData: any) {
-    const response = await axios.post(
-      `${this.asaasApiUrl}/payments`,
-      chargeData,
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          access_token: this.asaasApiKey,
+  async createCharge(chargeData: CreateChargeDto) {
+    try {
+      const response = await axios.post(
+        `${this.asaasApiUrl}/payments`,
+        chargeData,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            access_token: this.asaasApiKey,
+          },
         },
-      },
-    );
-    return response.data;
+      );
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(
+        'Erro ao criar o cobrança',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   async handleWebhook(data: any) {
